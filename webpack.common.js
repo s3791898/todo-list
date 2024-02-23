2954eb17397e115b31a9602b2740bdf961e7d34d
Anthony Tsui <68526943+s3791898@users.noreply.github.com.>
2024-02-24 08:58:13 +1100

    added eslint and babel

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
+
+  // define entry point
   entry: "./src/index.js",
   devtool: "inline-source-map",
   plugins: [
@@ -14,6 +16,8 @@ module.exports = {
       scriptLoading: "defer",
     }),
   ],
+
+  // define output point
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
@@ -33,6 +37,16 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
       },
     ],
   },
